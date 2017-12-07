6ea0403518812dba80f37d1d416456b5d1f41fb7
rubykdavis <rubykdavis@gmail.com>
2017-12-07 15:00:10 -0800

    adding dict of flickr names bc SOMEONE (looks at UW class services) couldn't name them consistently

---
+++ building-dict.py
@@ -0,0 +1,47 @@
+buildings = {
+    "Anderson"
+    "Architecture"
+    Art
+    Bagley
+    Benson
+    Bloedel
+    Chemistry Library
+    Clark
+    Communications
+    Condon
+    Dempsey
+    Denny
+    Eagleson
+    Electrical Engineering
+    Executive Education
+    Fisheries Sciences
+    Fisheries Teaching
+    Gould
+    Gowen
+    Guggenheim
+    Hitchcock
+    Johnson
+    Kane
+    Loew
+    Marine Studies
+    Mary Gates
+    Mechanical Engineering
+    Miller
+    More
+    Mueller
+    Music
+    Observatory
+    Ocean Teaching
+    OUGL
+    PACCAR
+    Parrington
+    Physics and Astronomy
+    Raitt
+    Savery
+    Sieg
+    Smith
+    Social Work
+    Thomson
+    Winkenwerder
+
+}
