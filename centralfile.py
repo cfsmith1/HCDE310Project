0c0904462f7386aa8ac1fda87a0843ebfe00d464
Courtney Faye Smith <CourtneySmith@D-108-179-189-118.dhcp4.washington.edu>
2017-12-07 10:53:42 -0800

    test

--- centralfile.py
+++ centralfile.py
@@ -1,5 +1,7 @@
 import json, jinja2, RMP_API, flickr_albums, os, webbrowser
 
+#hi rubby 
+
 JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
